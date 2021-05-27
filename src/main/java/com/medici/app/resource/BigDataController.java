6bf37430d49071f704e7ad8b41a871b344a60db8
TOSS <tiago.medice@gft.com>
2021-05-27 13:53:32 +0200

    fix

--- src/main/java/com/medici/app/resource/BigDataController.java
+++ src/main/java/com/medici/app/resource/BigDataController.java
@@ -15,7 +15,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.api.gax.paging.Page;
+import com.google.cloud.bigquery.BigQuery.DatasetListOption;
+import com.google.cloud.bigquery.BigQuery.TableListOption;
+import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableResult;
 import com.medici.app.entity.QueryMessage;
 import com.medici.app.service.BigQueryService;
 
@@ -59,6 +65,53 @@ public class BigDataController {
 		try {
 			ListenableFuture<Job> payloadJob = bigQueryService.writeDataToTable(uploadfile, tableName);
 			return new ResponseEntity(payloadJob, new HttpHeaders(), HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@RequestMapping(value = "/{dataset}/{table}/{pageSize}", method = RequestMethod.GET)
+	public TableResult listTableData(@PathVariable("dataset")  String dataset ,@PathVariable("table")  String table, @PathVariable("pageSize")  long pageSize) {
+
+			TableResult payload = bigQueryService.listTableData(dataset, table, pageSize);
+			return payload;
+
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<?> getDatasetName(  ) {
+
+		try {
+			String payload = bigQueryService.getDatasetName();
+			return new ResponseEntity(payload, new HttpHeaders(), HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@RequestMapping(value = "/datasets", method = RequestMethod.POST)
+	public ResponseEntity<?> listDatasets(@RequestBody DatasetListOption options) {
+
+		try {
+			Page<Dataset> payload = bigQueryService.listDatasets(options);
+			return new ResponseEntity(payload, new HttpHeaders(), HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@RequestMapping(value = "/dataset/{datasetId}", method = RequestMethod.POST)
+	public ResponseEntity<?> listTables(@PathVariable("datasetId") String datasetId, @RequestBody TableListOption options) {
+
+		try {
+			Page<Table> payload = bigQueryService.listTables(datasetId, options);
+			return new ResponseEntity(payload, new HttpHeaders(), HttpStatus.OK);
 
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
--- src/main/java/com/medici/app/service/BigQueryService.java
+++ src/main/java/com/medici/app/service/BigQueryService.java
@@ -9,8 +9,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.api.gax.paging.Page;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQuery.DatasetListOption;
+import com.google.cloud.bigquery.BigQuery.TableDataListOption;
+import com.google.cloud.bigquery.BigQuery.TableListOption;
+import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.FormatOptions;
 import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.bigquery.TableResult;
 import com.medici.app.config.AutoConfiguration.BigQueryFileGateway;
 
 @Service
@@ -22,16 +31,35 @@ public class BigQueryService {
 	@Autowired
 	BigQueryTemplate bigQueryTemplate;
 
+	@Autowired
+	BigQuery bigquery;
+
 	@Value("${spring.cloud.gcp.bigquery.datasetName}")
 	private String datasetName;
 
+	public String getDatasetName() throws IOException {
+		return this.bigQueryTemplate.getDatasetName();
+	}
+
 	public ListenableFuture<Job> writeDataToTable(MultipartFile file, String tableName) throws IOException {
 		return this.bigQueryTemplate.writeDataToTable(tableName, file.getInputStream(), FormatOptions.csv());
-
 	}
 
 	public ListenableFuture<Job> insertBigQueryTable(String csvData, String tableName) {
 		return this.bigQueryFileGateway.insertBigQueryTable(csvData.getBytes(), tableName);
+	}
+
+	public Page<Dataset> listDatasets(DatasetListOption options) {
+		return bigquery.listDatasets(options);
+	}
+
+	public Page<Table> listTables(String datasetId, TableListOption options) {
+		return bigquery.listTables(datasetId, options);
+	}
+
+	public TableResult listTableData(String dataset, String table, long pageSize) {
+		TableId tableId = TableId.of(dataset, table);
+		return bigquery.listTableData(tableId, TableDataListOption.pageSize(pageSize));
 	}
 
 }
